7e2a71a73a9c94f5512234e899ac0b089fc01f6c
joaobenthin <joaopaulobenthin@hotmail.com>
2020-10-17 12:19:42 -0300

    Add joaobenthin profile

--- people/data.js
+++ people/data.js
@@ -7,6 +7,14 @@ const data = [
       'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
     folderUrl: 'TEMPLATE'
   },
+  {
+    name: 'João Benthin',
+    image: '/people/joaobenthin/profile.jpg',
+    title: 'Full Stack Developer',
+    description:
+      'Programador apaixonado por tecnologia, aprendendo a empreender e dançarino nas horas vagas',
+    folderUrl: 'joaobenthin'
+  },
 ];
 
 export default data
