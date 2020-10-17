8eaf1015ac28a623066aa23c37cffb22557d9743
Luckened <luccafmsilva@gmail.com>
2020-10-17 12:41:50 -0300

    feat(people): add lucca =)

--- people/data.js
+++ people/data.js
@@ -7,6 +7,14 @@ const data = [
       'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
     folderUrl: 'TEMPLATE'
   },
+  {
+    name: 'Lucca Miranda',
+    image: '/people/lucca/profile.jpg',
+    title: 'Estudante e estagiario de frontend',
+    description:
+      'Sou amante de repteis, musica e codigo =).',
+    folderUrl: 'lucca',
+  },
 ];
 
 export default data
