0b5424890b2d81b3078c460816da9e933a1ace3a
Andressa Gontijo <dessagontijo@gmail.com>
2020-10-17 12:22:19 -0300

    Adding AndressaGontijo

--- script/app.test.js
+++
@@ -1,9 +0,0 @@
-import data from "../people/data.js";
-
-it("should not pass the characters limit", () => {
-  const fields = data.filter((e) =>
-    Object.values(e).some((i) => i.length > 140)
-  );
-
-  expect(fields.length).toBe(0);
-});
