78d54e682573a1255ff51b5f23c63fd31f1577b6
Felipe Souza <fnasci@gmail.com>
2020-10-17 12:35:31 -0300

    including myself

--- people/data.js
+++ people/data.js
@@ -7,6 +7,14 @@ const data = [
       'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
     folderUrl: 'TEMPLATE'
   },
+  {
+    name: 'Felipe Souza',
+    image: '/people/felipe-souza/felipe-image.jpg',
+    title: 'Analista de Tecnologia no SENAI/MG',
+    description:
+      'Atuo no SENAI com foco em transformação para Indústria 4.0. Atualmente, desenvolvendo para web com tecnologias em JS.',
+    folderUrl: 'felipe-souza'
+  },
 ];
 
 export default data
