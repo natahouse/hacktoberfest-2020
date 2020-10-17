6182aa0948bbcc9d9b288eab3bc062d6b125cdd8
Ana Reis <analuizareism@hotmail.com>
2020-10-17 12:48:24 -0300

    Add Ana Luiza

--- people/data.js
+++ people/data.js
@@ -7,6 +7,14 @@ const data = [
       'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
     folderUrl: 'TEMPLATE'
   },
+  {
+    name: 'Ana Luíza Reis',
+    image: 'https://avatars3.githubusercontent.com/u/44124220?s=460&u=ee600f44f31a89e312b6c795d877888fc320d7c8&v=4',
+    title: 'Segundo periodo de ADS',
+    description:
+      'Estou muito feliz de participar do meu segundo Haktoberfest na Nata!',
+    folderUrl: 'Ana-Luiza-Reis'
+  },
 ];
 
 export default data
