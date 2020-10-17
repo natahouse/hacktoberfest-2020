d9b88e49e0081337baa04fd3ce1846fadfcf0154
Arthur Lima <arthur-esc@hotmail.com>
2020-10-17 12:21:53 -0300

    adicionado arthur lima

--- people/data.js
+++ people/data.js
@@ -7,6 +7,14 @@ const data = [
       'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
     folderUrl: 'TEMPLATE'
   },
+  {
+    name: 'Arthur Lima',
+    image: '/people/arthurlima/profile.jpeg',
+    title: 'Desenvolvedor de software',
+    description:
+      'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
+    folderUrl: 'arthurlima'
+  },
 ];
 
 export default data
