6689174cec3f47ee1c6a69b8b06d378c9ce1b149
João Artur <67400301+joaothomaz@users.noreply.github.com>
2020-10-17 12:22:29 -0300

    add joao artur

--- people/data.js
+++ people/data.js
@@ -7,6 +7,14 @@ const data = [
       'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
     folderUrl: 'TEMPLATE'
   },
+  {
+    name: 'João Artur',
+    image: '/people/joaoartur/profile.jpeg',
+    title: 'Fala Dev!',
+    description:
+      'Engenheiro, entusiasta das tecnologias para web e tentando ser atleta nas horas vagas!',
+    folderUrl: 'joaoartur'
+  },
 ];
 
 export default data
