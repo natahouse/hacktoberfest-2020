92782b5dd3491cdc615dc6f04c887bd1552fef0c
Pedroemanuelavelar <pedroemanuel5296@gmail.com>
2020-10-17 12:58:36 -0300

    Adicionado estilos e perfil Pedro Emanuel

--- people/data.js
+++ people/data.js
@@ -7,6 +7,14 @@ const data = [
       'Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source o/.',
     folderUrl: 'TEMPLATE'
   },
+  {
+    name: 'Pedro Emanuel',
+    image: '/people/Pedro-Emanuel/profile.jpeg',
+    title: 'Desenvolvedor Front End',
+    description:
+      'Entusiasta e apaixonada por desenvolvimento web, mundo front End, como Mailto Gai dizia se não nasci com um dom natural, tenho que me esforçar 500 vezes mais! ',
+    folderUrl: 'Pedro-Emanuel'
+  },
 ];
 
 export default data
